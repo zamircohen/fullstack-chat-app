9d2c6b5144591a9cda8116192a88c08a6d19b6fb
zamircohen <zamir_cohen@hotmail.com>
2022-10-03 13:35:58 +0200

    Add frontend functionality

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TodoItem from '../../shared/src/chat-item';
+import "./App.css";
+import axios from "axios";
+import { BsTrash } from "react-icons/bs";
+// import { AiOutlineEdit } from "react-icons/ai";
+
+axios.defaults.baseURL = process.env.REACT_APP_TODO_API || 'http://localhost:3001'
+
+const fetchTodos = async (): Promise<TodoItem[]> => {
+  const response = await axios.get<TodoItem[]>('/todos')
+  return response.data
+}
 
 function App() {
+  const [todoText, setTodoText] = useState<string>('')
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [error, setError] = useState<string | undefined>();
+  
+  const createTodo = async (todoText: string): Promise<void> => {
+    const todoItem: TodoItem = {
+      text: todoText,
+      timeStamp: new Date()
+    }
+  
+  try {
+    await axios.post<TodoItem[]>('/todos', todoItem) 
+    const response = await axios.get<TodoItem[]>('/todos')
+    setTodos(response.data)
+    setTodoText('')
+  } catch (err) {
+    setTodos([])
+    setError('Error creating todo')
+  }
+  }
+
+  const deleteTodo = async (todo: TodoItem): Promise<void> => {
+    try {
+      await axios.delete<TodoItem[]>(`/todos/${todo._id}`)
+      const response = await axios.get<TodoItem[]>('/todos')
+      setTodos(response.data)
+    } catch (err) {
+      setTodos([])
+      setError('Error deleting todo')
+    }
+  }
+
+  
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchTodos()
+        .then(setTodos)
+        .catch((error) => {
+          setTodos([])
+          setError('Something went wrong while searching for my todos...')
+      })
+    }, 2000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const output = () => {
+    if (error) {
+      return (<div>{error}</div>)
+    } else if (todos) {
+      return (<div>{
+        todos.map((item) => {
+          return (
+            <>
+            <div className="Todo">
+            <span key={item._id}>{item.text} 
+            <button className="Delete_Button" onClick={() => deleteTodo(item)}> < BsTrash/> </button> 
+            {/* <button className="Edit_Button"> < AiOutlineEdit/> </button>
+            <input type="text" name="popup" id="popup" className="hide"></input> <button name="popup" id="popup" className="hide">Edit</button> */}
+            </span>
+            </div>
+            </>
+          )
+        })
+        }</div>)
+    } else 
+      (<div>'Waiting for todos'</div>)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Header">
+        <br />
+        My todos...
+        </div>
+        <div className="Output-box">
+        {output()}
+        </div>
       </header>
+      <div className="Bottom_Field">
+        <input className="Input_Field" placeholder={`Your new todo item...`} minLength={1} maxLength={20} type="text" value={todoText} onChange={(e) => setTodoText(e.target.value)}/>
+        <br />
+        <button disabled={!todoText} className="Create_Button" onClick={(e) => createTodo(todoText)}>Create todo</button>
+      </div>
     </div>
   );
 }
