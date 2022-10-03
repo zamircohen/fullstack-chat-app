42ea0679a4ab77f8c70ee4b4e883e8d3e6b37281
zamircohen <zamir_cohen@hotmail.com>
2022-10-03 13:29:37 +0200

    Add backend boiler plate

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, json, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import {setupMongoDb} from './db';
 import cors from 'cors';
+
+dotenv.config()
 
 const app: Application = express()
 app.use(cors()) 
 app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || '3001')
+const mongoURL: string = process.env.MONGO_URL || 'mongodb://localhost:27017/chat'
 
-app.get('/hello', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+app.use("/chats", todosController)
 
-app.listen(port, function () {
-    console.log(`Example app listening ${port}`)
+app.listen(port, async function() {
+    await setupMongoDb(mongoURL)
+    console.log('App is running on port ' + port)
 })
