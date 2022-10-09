46a8f570beaa865a69a8fcd7cda542200f87228a
zamircohen <zamir_cohen@hotmail.com>
2022-10-09 15:44:03 +0200

    Add styling to chat messages

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ChatItem from "../../shared/src/chat-item";
 import "./App.css";
 import axios from "axios";
