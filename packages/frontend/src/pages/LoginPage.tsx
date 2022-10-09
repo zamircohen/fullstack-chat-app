934cc79bfdce844ef6a02b45806729b44c5a20c3
zamircohen <zamir_cohen@hotmail.com>
2022-10-09 11:52:39 +0200

    Create login page with functionality

--- packages/frontend/src/pages/LoginPage.tsx
+++ packages/frontend/src/pages/LoginPage.tsx
@@ -1,41 +1,64 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate()
-
-    function login(username: string, password: string) {
-        // e.preventDefault()
-        const url = "http://localhost:3001/login"
-        const payload = { username, password }
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(payload)
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.token) {
-                    localStorage.setItem("token", data.token)
-                    navigate("/chats")
-                }
-                else if (data.error) {
-                    alert("Wrong username or password")
-    }
+  function login(username: string, password: string) {
+    // e.preventDefault()
+    const url = "http://localhost:3001/login";
+    const payload = { username, password };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
     })
-    }
-    
-    return (
-        <div>
-            <h1>Login</h1>
-            <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={(e) => login(username, password)}>Login</button>
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          navigate("/chats");
+        } else if (data.error) {
+          alert("Wrong username or password");
+        }
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Header">
+          Login
         </div>
-    )
+        <div className="Output-box">
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button onClick={(e) => login(username, password)}>Login</button>
+        </div>
+
+        <div className="Bottom_Field">
+            <button onClick={() => navigate("/register")}>Register</button>
+        </div>
+        
+      </header>
+    </div>
+  );
 }
+
---
+++ packages/frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function SignupPage() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    function handleOnSubmit(e: any){
+        e.preventDefault()
+        
+        const payload = {
+            username,
+            password,
+        }
+        const url = "http://localhost:3001/create"
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        navigate("/")    
+    }
+
+
+  return (
+    <div>
+        <h1>Create account</h1>
+        <form onSubmit={handleOnSubmit}>
+                Username: <input 
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                /> <br />
+                Password: <input 
+                    type="password" 
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                /> <br />
+                <button type="submit">Sign up</button>
+           </form>
+           <br />
+           <a href="/">Back to login</a>
+    </div>
+  )
+}
